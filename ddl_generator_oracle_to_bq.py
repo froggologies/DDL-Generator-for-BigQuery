257c8c8f2a6d2768de07d715e6f8b84a5d73ba2c
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2024-10-23 17:18:47 +0700

    feat(ddl_generator_oracle_to_bq.py): add count_unknown_data_type variable to track unknown data types during conversion refactor(ddl_generator_oracle_to_bq.py): simplify table creation logic by removing dataset_name and project_id parameters feat(ddl_generator_oracle_to_bq.py): add timestamp to generated DDL info for tracking feat(ddl_generator_oracle_to_bq.py): add total unknown data type count to generated DDL info

--- ddl_generator_oracle_to_bq.py
+++ ddl_generator_oracle_to_bq.py
@@ -2,39 +2,39 @@ import csv
 import argparse
 from datetime import datetime
 
+count_unknown_data_type = 0
+
 
 def convert_data_type(data_type, data_length, data_precision, data_scale):
     """Converts data types from Oracle to BigQuery."""
+    global count_unknown_data_type
     data_type = data_type.upper()
-    if data_type == "VARCHAR2":
+    if data_type == "VARCHAR2" or data_type == "CHAR":
         return f"STRING({data_length})"
     elif data_type == "NUMBER":
-        if not data_precision:
-            data_precision = "1"
-        if not data_scale:
-            data_scale = "0"
+        data_precision = data_precision or "1"
+        data_scale = data_scale or "0"
         if int(data_precision) >= 30 or int(data_scale) >= 10:
             return f"BIGNUMERIC({data_precision}, {data_scale})"
         return f"NUMERIC({data_precision}, {data_scale})"
     elif data_type == "FLOAT":
         return f"FLOAT({data_precision}, {data_scale})"
-    elif data_type == "CHAR":
-        return f"STRING({data_length})"
-    elif data_type == "DATE":
-        return "DATE"
     elif data_type == "BLOB":
         return "BYTES"
     elif data_type == "CLOB":
         return "STRING"
     elif data_type == "RAW":
         return "BYTES"
+    elif data_type == "DATE":
+        return "DATE"
     elif data_type == "TIMESTAMP(6)":
         return "TIMESTAMP"
     else:
+        count_unknown_data_type += 1
         return f"STRING -- Type unknown"  # Default to STRING for unknown types
 
 
-def generate_ddl(csv_file, dataset_name, project_id):
+def generate_ddl(csv_file):
     """Generates BigQuery DDL from a CSV file."""
     ddl = ""
     count_table = 0
@@ -44,20 +44,12 @@ def generate_ddl(csv_file, dataset_name, project_id):
         current_table = ""
         count_table_column = 0
         for row in reader:
-            table_name = row["TABLE_NAME"].replace("$", "").lower()
+            table_name = row["BQ_ODS"]
             if table_name != current_table:
                 if current_table:
                     ddl += f"); -- Column: {str(count_table_column)}\n\n"  # Close previous table definition
                     count_table_column = 0
-                if dataset_name:
-                    if project_id:
-                        ddl += f"CREATE OR REPLACE TABLE `{project_id}.{dataset_name}.{table_name}` (\n"
-                    else:
-                        ddl += (
-                            f"CREATE OR REPLACE TABLE `{dataset_name}.{table_name}` (\n"
-                        )
-                else:
-                    ddl += f"CREATE OR REPLACE TABLE `{table_name}` (\n"
+                ddl += f"CREATE OR REPLACE TABLE `{table_name}` (\n"
                 current_table = table_name
                 count_table += 1
             column_name = row["COLUMN_NAME"].replace("$", "")
@@ -67,7 +59,7 @@ def generate_ddl(csv_file, dataset_name, project_id):
                 row["DATA_PRECISION"],
                 row["DATA_SCALE"],
             )
-            if row["NULLABLE"] == "N":
+            if row["NULLABLE"] == "false":
                 nullable = "NOT NULL"
                 ddl += f"  `{column_name}` {data_type} {nullable},\n"
             else:
@@ -75,29 +67,27 @@ def generate_ddl(csv_file, dataset_name, project_id):
             count_total_column += 1
             count_table_column += 1
         ddl += f"); -- Column: {str(count_table_column)}"  # Close the last table definition
-    ddl_info = f"/*\nTotal table: {str(count_table)}\nTotal column: {str(count_total_column)}\n*/\n\n"
+    iso_date = datetime.now().isoformat()
+    ddl_info = f"/*\nGenerated at: {iso_date}\nTotal table: {str(count_table)}\nTotal column: {str(count_total_column)}\nTotal unknown data type: {str(count_unknown_data_type)}\n*/\n\n"
     ddl = ddl_info + ddl
     return ddl
 
 
 if __name__ == "__main__":
 
-    # Format the current date and time as "YYYY-MM-DD-HH-MM-SS"
     formatted_date = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
 
     parser = argparse.ArgumentParser(
         description="Generate BigQuery DDL from a CSV file."
     )
     parser.add_argument("csv_file", help="Path to the CSV file.")
-    parser.add_argument("--dataset_name", help="Name of the BigQuery dataset to use.")
-    parser.add_argument("--project_id", help="Project ID to use.")
     args = parser.parse_args()
 
     # Generate the DDL file
-    ddl_output = generate_ddl(args.csv_file, args.dataset_name, args.project_id)
+    ddl_output = generate_ddl(args.csv_file)
 
     # Save the DDL to a file
-    output_filename = f"{args.csv_file.split('.')[0]}_ddl-{formatted_date}.sql"
+    output_filename = f"{args.csv_file.split('.')[0]}_{formatted_date}.sql"
     with open(output_filename, "w") as outfile:
         outfile.write(ddl_output)
     print(f"DDL saved to: {output_filename}")
